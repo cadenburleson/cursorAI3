6e5a07715a68399e87fdad0840e57468d4f4159d
Caden Burleson <devcaden@gmail.com>
2024-12-19 01:49:17 -0600

    wtf-pt5

--- build.sh
+++ build.sh
@@ -2,11 +2,3 @@
 
 # Run the Vite build directly
 npx vite build
-
-# Ensure we're in the right directory
-cd src
-
-# Move the dist directory up one level if it exists in src
-if [ -d "dist" ]; then
-    mv dist ../dist
-fi 
