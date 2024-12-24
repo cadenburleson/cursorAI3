6fe83ba65a94ee03b98eade6b9ef34a82c1e7ef8
Caden Burleson <devcaden@gmail.com>
2024-12-24 04:41:33 -0600

    Working?

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: 'src',
+    envDir: '../',
     build: {
         outDir: '../dist',
         emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: '/index.html',
+                login: '/login.html',
+                signup: '/signup.html',
+                resetPassword: '/reset-password.html',
+                changePassword: '/change-password.html'
+            }
+        }
     },
     server: {
         port: 3000,
